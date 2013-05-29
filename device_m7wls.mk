cee00f63e6e884b7bbfd3698554e822e34821019
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-29 14:52:27 +0200

    m7wls: build nfc hal from sources
    Change-Id: Iceeaebe23f0b5ec47d35feb96ded2296c85626bd

--- device_m7wls.mk
+++ device_m7wls.mk
@@ -23,6 +23,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
+# NFC
+PRODUCT_PACKAGES += \
+	nfc.m7wls
+
 ## CDMA Sprint stuffs
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-sprint-us \
